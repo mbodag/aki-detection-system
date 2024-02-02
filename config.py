3fc6e31765f5f9828ee908638c994dd2d6b6b742
Gordian Gruentuch <glg23@ic.ac.uk>
2024-02-02 23:05:04 +0000

    Updated config file and .gitignore

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 # config.py
 
 # The path to the CSV file where historical patient data is stored.
-HOSPITAL_DATA_CSV_PATH = 'path/to/hospital_data.csv'
+HISTORY_CSV_PATH = 'history.csv'
+HOSPITAL_DATA_CSV_PATH = 'hospital_data.csv'
 
 # Details for the message listener (e.g., IP and port for HL7 messages)
 HL7_LISTENER_IP = '127.0.0.1'
 HL7_LISTENER_PORT = 1234
+
+# The URI for the database (SQLite in this example)
+DATABASE_URI = 'sqlite:///patient_data.db'
 
 # Add any other global configuration variables here.
